2722a78a652eac344c19a8da61f07fb93d92a771
范书 <12743339+model-book@user.noreply.gitee.com>
2025-03-31 10:46:00 +0000

    增加RC4前端代码实现
    Signed-off-by: 范书 <12743339+model-book@user.noreply.gitee.com>

---
+++ RC4widget.h
@@ -0,0 +1,44 @@
+﻿#ifndef RC4WIDGET_H
+#define RC4WIDGET_H
+
+#include "mainwindow.h"
+#include "RC4.h"
+
+using namespace std;
+
+namespace Ui { class RC4widget; }
+
+class RC4widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit RC4widget(QWidget *parent = 0);
+    ~RC4widget();
+
+private slots:
+
+    void on_genpushButton_clicked();
+
+    void on_filepushButton_clicked();
+
+    void on_keypushButton_clicked();
+
+    void on_encryptpushButton_clicked();
+
+    void on_decryptpushButton_clicked();
+
+    void on_backpushButton_clicked();
+
+signals:
+    void sendsignal();
+
+private:
+    string infile_path;
+    string key_path;
+
+    RC4 rc4;
+    Ui::RC4widget *ui;
+};
+
+#endif // RC4WIDGET_H
