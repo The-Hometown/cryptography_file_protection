90d81fe5fc0a28fb8039ffd0f23e16c47546ca39
qingfengfumeng <502355407@qq.com>
2025-03-31 10:30:42 +0000

    增加DES前端代码设计
    Signed-off-by: qingfengfumeng <502355407@qq.com>

---
+++ DESwidget.h
@@ -0,0 +1,43 @@
+﻿#ifndef DESWIDGET_H
+#define DESWIDGET_H
+
+#include "mainwindow.h"
+#include "DES.h"
+
+using namespace std;
+
+namespace Ui { class DESwidget; }
+
+class DESwidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit DESwidget(QWidget *parent = 0);
+    ~DESwidget();
+
+private slots:
+
+    void on_genpushButton_clicked();
+
+    void on_filepushButton_clicked();
+
+    void on_keypushButton_clicked();
+
+    void on_encryptpushButton_clicked();
+
+    void on_decryptpushButton_clicked();
+
+    void on_backpushButton_clicked();
+
+signals:
+    void sendsignal();
+
+private:
+    string infile_path;
+    string key_path;
+    DES des;
+    Ui::DESwidget *ui;
+};
+
+#endif // DESWIDGET_H
