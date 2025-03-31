5c062b3f29d0219a98a79e02c3d8b78c39ecdf3a
qingfengfumeng <502355407@qq.com>
2025-03-31 11:21:53 +0000

    增加ZUC前端代码设计
    Signed-off-by: qingfengfumeng <502355407@qq.com>

---
+++ ZUCwidget.h
@@ -0,0 +1,44 @@
+﻿#ifndef ZUCWIDGET_H
+#define ZUCWIDGET_H
+
+#include "mainwindow.h"
+#include "ZUC.h"
+
+using namespace std;
+
+namespace Ui { class ZUCwidget; }
+
+class ZUCwidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ZUCwidget(QWidget *parent = 0);
+    ~ZUCwidget();
+
+private slots:
+
+    void on_genpushButton_clicked();
+
+    void on_filepushButton_clicked();
+
+    void on_keypushButton_clicked();
+
+    void on_encryptpushButton_clicked();
+
+    void on_decryptpushButton_clicked();
+
+    void on_backpushButton_clicked();
+
+signals:
+    void sendsignal();
+
+private:
+    string infile_path;
+    string key_path;
+
+    ZUC zuc;
+    Ui::ZUCwidget *ui;
+};
+
+#endif // ZUCWIDGET_H
